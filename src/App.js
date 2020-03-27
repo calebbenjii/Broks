e482f8e0b81be128b496d20910fcba45806a552e
kaiylebdev <calebben94@gmail.com>
2020-03-27 13:08:49 +0100

    folder and page structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/HomePage'
+import About from './pages/AboutPage'
+import Cart from './pages/CartPage'
+import Products from './pages/ProductsPage'
+import SingleProduct from './pages/SingleProductPage'
+import Contact from './pages/ContactPage'
+import Default from './pages/DefaultPage'
 
 
 
